332762e4a823271ea94da6b5bbb332786e83c622
nazri <nazrawigedion23@gmail.com>
2025-10-29 13:57:12 +0300

    passanger

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -6,7 +6,7 @@ import sys
 
 
 from django.core.wsgi import get_wsgi_application
-sys.path.append('/bdn_ftp/tenabot/')
+sys.path.append('/bdnsys.com/nazri_bdn/tena/')
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tenabot.settings')
 application = get_wsgi_application()
 
