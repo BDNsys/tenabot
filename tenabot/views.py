cab36862ad793553098c6829cf6b5af808ec79d6
nazri <nazrawigedion23@gmail.com>
2025-10-30 11:32:25 +0300

    frontend fix

--- tenabot/views.py
+++ tenabot/views.py
@@ -1,41 +1,24 @@
 
 
-from django.views.decorators.csrf import csrf_exempt
-from django.http import JsonResponse
+
 from users.models import User
-import json
+
+
+from asgiref.sync import sync_to_async
 
 
 
-@csrf_exempt
-def register_telegram_user(request):
-    if request.method == "POST":
-        data = json.loads(request.body)
-        telegram_id = data.get("telegram_id")
-
-        user, created = User.objects.get_or_create(
-            telegram_id=telegram_id,
-            defaults={
-                "username": data.get("username") or f"user_{telegram_id}",
-                "first_name": data.get("first_name", ""),
-                "last_name": data.get("last_name", ""),
-            },
-        )
-
-        return JsonResponse({
-            "success": True,
-            "created": created,
-            "user": {
-                "id": user.id,
-                "telegram_id": user.telegram_id,
-                "username": user.username,
-                "first_name": user.first_name,
-                "last_name": user.last_name,
-            }
-        })
-    return JsonResponse({"error": "Invalid request"}, status=400)
-
-
+@sync_to_async
+def register_telegram_user(telegram_user):
+    user, created = User.objects.get_or_create(
+        telegram_id=telegram_user.id,
+        defaults={
+            "username": telegram_user.username or f"user_{telegram_user.id}",
+            "first_name": telegram_user.first_name or "",
+            "last_name": telegram_user.last_name or "",
+        },
+    )
+    return user, created
 
 
 
