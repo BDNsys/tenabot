390e05716841dc6d556df6de1b419e444b918b48
nazri <nazrawigedion23@gmail.com>
2025-11-01 02:56:07 +0300

    media

--- tenabot/urls.py
+++ tenabot/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.urls import re_path
+from django.conf.urls.static import static,serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,3 +28,8 @@ urlpatterns = [
     path('api/get_user/<str:telegram_id>/', views.get_user, name='get_user'),
     path('api/get_users/', views.get_users, name='get_users'),
 ]
+if settings.DEBUG:
+   urlpatterns +=static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+else:
+    urlpatterns+= [ re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT})]
+    urlpatterns+= [re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT})]
